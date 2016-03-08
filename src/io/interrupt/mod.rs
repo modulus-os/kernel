20e60cf813b3e607ecbd79a6f274dd4a17023802
IceMatrix <epicpotatopro@gmail.com>
2016-03-08 22:43:39 +0000

    Int config

--- src/io/interrupt/mod.rs
+++ src/io/interrupt/mod.rs
@@ -6,6 +6,8 @@
 
 pub mod keyboard;
 
+use io::cpuio::Port;
+
 pub struct Entry {
 	offset_low: u16,
 	selector: u16,
@@ -36,9 +38,53 @@ pub struct IDTR {
 	offset: u64,
 }
 
+impl IDTR {
+	pub fn new(limit: u16, offset: u64) -> IDTR {
+		IDTR {limit: limit, offset: offset}
+	}
+}
+
 pub fn init_idt() {
-	let kb_addr = asm_kb_handler as *const usize;
-	//let kb_entry = Entry::new();
+	let kb_addr: *const ();
+	unsafe {
+		kb_addr = asm_kb_handler as *const ();
+	}
+
+	let mut idt: [Entry; 1] = [
+		Entry::new(kb_addr as u16 & 0xffff, 0x08, 0x8e,
+			(kb_addr as u16 & 0xffff0000) >> 8,
+			(kb_addr as u32 & 0xffffffff00000000) >> 16)
+	];
+
+	let icw1_1 = Port::new(0x20);
+	let icw1_2 = Port::new(0xA0);
+	let icw234_1 = Port::new(0x21);
+	let icw234_2 = Port::new(0xA1);
+
+	//Start config
+	icw1_1.outb(0x11);
+	icw1_2.outb(0x11);
+
+	//
+	icw234_1.outb(0x20);
+	icw234_2.outb(0x28);
+
+	icw234_1.outb(0x00);
+	icw234_2.outb(0x00);
+
+	icw234_1.outb(0x01);
+	icw234_2.outb(0x01);
+
+	icw234_1.outb(0xff);
+	icw234_2.outb(0xff);
+
+	//unsafe {idt_addr = *(idt[0] as *const Entry);}
+	//let idtr = IDTR::new((12 * idt.len()) as u16, idt_addr as u64);
+
+	lidt();
+}
+
+fn lidt() {
 }
 
 //Assembly interrupt wrappers

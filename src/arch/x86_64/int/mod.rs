7476ead81fea7b22e8b5a756a014cb2fde26fdba
IceMatrix <epicpotatopro@gmail.com>
2016-04-18 22:28:43 -0500

    IDT work

--- src/arch/x86_64/int/mod.rs
+++ src/arch/x86_64/int/mod.rs
@@ -10,15 +10,15 @@ pub struct Entry {
 }
 
 impl Entry {
-	pub fn new(address: u64, selector: u16, typ_attr: u8) -> Self {
-		Entry {
-			offset_low: (address & 0x000000000000ffff) as u16,
-			selector: selector,
-			zero1: 0,
-			typ_attr: typ_attr,
-			offset_mid: ((address & 0x00000000ffff0000) >> 16) as u16,
-			offset_high: ((address & 0xffffffff00000000) >> 32) as u32,
-			zero2: 0,
-		}
-	}
+    pub fn new(address: u64, selector: u16, typ_attr: u8) -> Self {
+        Entry {
+            offset_low: (address & 0x000000000000ffff) as u16,
+            selector: selector,
+            zero1: 0,
+            typ_attr: typ_attr,
+            offset_mid: ((address & 0x00000000ffff0000) >> 16) as u16,
+            offset_high: ((address & 0xffffffff00000000) >> 32) as u32,
+            zero2: 0,
+        }
+    }
 }

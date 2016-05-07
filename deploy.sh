42c5533921dcbbd916fe2629af741ebde60fb2b7
IceMatrix <epicpotatopro@gmail.com>
2016-05-07 13:36:34 -0500

    Circle CI

--- deploy.sh
+++ deploy.sh
@@ -4,19 +4,7 @@ cd modulus-os.github.io
 rm -rf doc
 cp -r ../target/doc .
 git config user.name "DocBot"
-git config user.email "$COMMIT_AUTHOR_EMAIL"
+git config user.email "epicpotatopro@gmail.com"
 git add .
-git commit -m "Docs: ${SHA}"
-
-# Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
-ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
-ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
-ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
-ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key.enc -out deploy_key -d
-chmod 600 deploy_key
-eval `ssh-agent -s`
-ssh-add deploy_key
-
-# Now that we're all set up, we can push.
-git push $SSH_REPO $TARGET_BRANCH
+git commit -m "Docs"
+git push -u origin master

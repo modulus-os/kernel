aa2e0741a7cea996120ca24efc4cdd7d6a67c432
IceMatrix <epicpotatopro@gmail.com>
2016-05-07 17:16:09 -0500

    Circle

--- deploy.sh
+++ deploy.sh
@@ -7,4 +7,10 @@ git config user.name "DocBot"
 git config user.email "epicpotatopro@gmail.com"
 git add .
 git commit -m "Docs"
+
+cp ~/.ssh/config ~/.ssh/config_old
+rm ~/.ssh/config
+echo "Host github.com\n IdentitiesOnly yes\n IdentityFile /home/ubuntu/.ssh/id_voxl" | cat > ~/.ssh/config
 git push
+rm ~/.ssh/config
+mv ~/.ssh/config_old ~/.ssh/config
